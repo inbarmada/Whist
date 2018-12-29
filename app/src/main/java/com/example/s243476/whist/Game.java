c44c5cac0f49195a623130acbf8827ee4bc22605
inbarmada <32581040+inbarmada@users.noreply.github.com>
2018-12-28 19:17:45 -0600

    Make cards into buttons

--- app/src/main/java/com/example/s243476/whist/Game.java
+++ app/src/main/java/com/example/s243476/whist/Game.java
@@ -1,8 +1,15 @@
 package com.example.s243476.whist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.*;
 
 public class Game extends AppCompatActivity {
 
@@ -16,7 +23,7 @@ public class Game extends AppCompatActivity {
         Deck theDeck = new Deck();
 
         //Create 4 hands
-        Hand one = new Hand(false);
+        final Hand one = new Hand(false);
         Hand two = new Hand(true);
         Hand three = new Hand(true);
         Hand four = new Hand(true);
@@ -25,9 +32,19 @@ public class Game extends AppCompatActivity {
         theDeck.deal(one, two, three, four);
 
         //Show one's hand
-        TextView tv = (TextView) findViewById(R.id.textView);
-        tv.setText(one.showCards().toString());
-    }
+        LinearLayout ll = (LinearLayout) findViewById(R.id.buttonlayout);
+        for(final Card i : one.showCards()) {
+            Button b = new Button(this);
+            b.setText(i.toString());
+            b.setOnClickListener(new OnClickListener(){
+                public void onClick(View arg0) {
+                    TextView tv = (TextView) findViewById(textView);
+                    tv.setText(one.choose(i).toString());
+                }
+            });
 
+            ll.addView(b);
+        }
+    }
 
 }
